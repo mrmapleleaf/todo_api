5e0617c12b1952fdf9a3fcde9f9db122f8900e11
mrmapleleaf <kaple.de.96@gmail.com>
2026-01-04 16:58:35 +0900

    詳細取得用エンドポイント実装

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import getToDoList, createToDo, getToDoDetail
 
 urlpatterns = [
-    path("todos/", getToDoList, name="getToDoList"),
-    path("todo/create/", createToDo, name="createToDo"),
+    path("todos", getToDoList, name="getToDoList"),
+    path("todo/create", createToDo, name="createToDo"),
+    # URLパターンとView関数のパラメータ名は同じである必要がある
     path("todo/<int:todo_id>", getToDoDetail, name="getToDoDetail"),
 ]
